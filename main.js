8d12876d457fe1bcb4c08fd005c46c80599ea093
Vigo-l <v.linders@sintlucasedu.nl>
2024-02-08 11:48:26 +0100

    movement

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 document.getElementById("mainTitle").innerText = "Point and Click game";
+
+const gameWindow = document.getElementById("gameWindow");
+const maincharacter = document.getElementById("maincharacter");
+
+gameWindow.onclick = function (e) {
+    var rect = gameWindow.getBoundingClientRect();
+    var x = e.clientX - rect.left;
+    var y = e.clientY - rect.top;
+    console.log(e.target.id);
+    maincharacter.style.left = x + "px";
+    maincharacter.style.top = y + "px";
+}
